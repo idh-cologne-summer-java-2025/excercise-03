1a5d74f4fcbb5917f568b6d884ce58fab229c1eb
benediktbenz <benediktbenz@localhost>
2025-05-09 16:36:38 +0200

    Aufgabe 1 erledigt + Token-Zähler am Ende

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -8,8 +8,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Document{
+public class Document implements Iterable<String>{
 	String documentText;
+	
+	
 
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
@@ -25,6 +27,19 @@ public class Document{
 		return doc;
 	}
 	
+	
+	public String Tokenizer(String s) {
+	
+
+	 StringTokenizer st = new StringTokenizer(s);
+    while (st.hasMoreTokens()) {
+        //System.out.println(st.nextToken());
+          s= s+st.nextToken();
+    }
+    	return s;
+	}
+
+	
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -33,11 +48,31 @@ public class Document{
 		this.documentText = documentText;
 	}
 	
+	
+	public Iterator<String> iterator() {
+		return new Iterator<String>() {
+			StringTokenizer st = new StringTokenizer(documentText);
+
+			public boolean hasNext() {
+				return st.hasMoreTokens();
+			}
+
+			public String next() {
+				return st.nextToken();
+			}
+		};
+	}
+	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
 		
+	int i = 0;
+	for (String token : d) {
+		System.out.println(token);
+		i=i+1;
 	}
+	System.out.println("Gesamte Token-Anzahl: " + i);
 
-	
+	}
 	
 }
