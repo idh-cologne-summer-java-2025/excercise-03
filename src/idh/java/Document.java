a473c3e31949f96af5c4eb394b6cddd5cee86be0
tjablons <tjablons@smail.uni-koeln.de>
2025-05-11 14:38:02 +0200

    Solved ex 1

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -8,36 +8,48 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Document{
-	String documentText;
+public class Document implements Iterable<String> {
+    private String documentText;
+    
+    private String readFromFile(File f) throws IOException {
+        FileReader fileReader = new FileReader(f);
+        int ch;
+        StringBuilder b = new StringBuilder();
+        while ((ch = fileReader.read()) != -1) {
+            b.append((char) ch);
+        }
+        fileReader.close();
+        return b.toString();
+    }
+   
+    public Document(File file) throws IOException {
+        this.documentText = readFromFile(file);
+    }
 
-	public static Document readFromFile(File f) throws IOException {
-		FileReader fileReader = new FileReader(f);
-		int ch;
-		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
-			b.append((char) ch);
-		}
-		fileReader.close();
-		Document doc = new Document();
-		doc.documentText = b.toString();
-		
-		return doc;
-	}
-	
-	public String getDocumentText() {
-		return documentText;
-	}
+    public String getDocumentText() {
+        return documentText;
+    }
 
-	public void setDocumentText(String documentText) {
-		this.documentText = documentText;
-	}
-	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		
-	}
+    @Override
+    public Iterator<String> iterator() {
+        StringTokenizer tokenizer = new StringTokenizer(documentText);
+        return new Iterator<String>() {
+            @Override
+            public boolean hasNext() {
+                return tokenizer.hasMoreTokens();
+            }
 
-	
-	
+            @Override
+            public String next() {
+                return tokenizer.nextToken();
+            }
+        };
+    }
+
+    public static void main(String[] args) throws IOException {
+        Document doc = new Document(new File("data/dracula.txt"));
+        for (String token : doc) {
+            System.out.print(token + " ");
+        }
+    }
 }
