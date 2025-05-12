bee9197461af7b56a7d01fa2012cd957b5586b00
gabriela <gabriela@gabrielas-air>
2025-05-12 20:28:30 +0200

    Abgabe Excersice 3

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -8,8 +8,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Document{
-	String documentText;
+public class Document implements Iterable<String> {
+    String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
@@ -32,12 +32,28 @@ public class Document{
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
+	 // Hier machen wir Document iterierbar:
+    @Override
+    public Iterator<String> iterator() {
+        // Wir erzeugen aus dem Text eine Liste von Tokens (z. B. Wörtern)
+        List<String> tokens = new LinkedList<>();
+        StringTokenizer tokenizer = new StringTokenizer(documentText);
+        while (tokenizer.hasMoreTokens()) {
+            tokens.add(tokenizer.nextToken());
+        }
+        return tokens.iterator(); // Gibt einen Iterator über die Liste zurück
+    }
 	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
 		
+		// Ausgabe der Tokens in einer for-each-Schleife:
+        for (String token : d) {
+            System.out.println(token);
+        }
 	}
+		
+}
 
 	
 	
-}
