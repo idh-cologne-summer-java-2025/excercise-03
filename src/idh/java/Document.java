302c4779bb751ca0e46e986b694222d4acfe441d
Lydiadinoki <elchkeks@schreiber-ehle.de>
2025-05-12 22:15:10 +0200

    Ex3 solution

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,15 +3,19 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Document{
+public class Document implements Iterable<String>{
 	String documentText;
+	 ArrayList<String> al;
+	 Iterator<String> it;
 
-	public static Document readFromFile(File f) throws IOException {
+	public Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
 		int ch;
 		StringBuilder b = new StringBuilder();
@@ -19,10 +23,11 @@ public class Document{
 			b.append((char) ch);
 		}
 		fileReader.close();
-		Document doc = new Document();
-		doc.documentText = b.toString();
-		
-		return doc;
+		this.documentText = b.toString();
+		 al = new ArrayList<>(Arrays.asList(documentText.split("[,\\.\\s+]")));
+		 it = al.iterator();
+		 
+		return this;
 	}
 	
 	public String getDocumentText() {
@@ -33,11 +38,21 @@ public class Document{
 		this.documentText = documentText;
 	}
 	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
+	
+
+	@Override
+	public Iterator<String> iterator() {
 		
-	}
+		return it;
+		
+		
+	    
+
 
 	
+	    
+	    		
+	    	}
 	
-}
+	}
+
