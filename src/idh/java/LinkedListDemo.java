3e7b620c071d368d9bb95ebb6625636400de8d0b
emil <emil@localhost>
2025-05-13 16:55:34 +0200

    zack

--- src/idh/java/LinkedListDemo.java
+++ src/idh/java/LinkedListDemo.java
@@ -2,7 +2,7 @@ package idh.java;
 
 public class LinkedListDemo {
 
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		MyLinkedList<String> ll = new MyLinkedList<String>();
 		ll.add("Hallo");
 		ll.add("Welt");
@@ -10,8 +10,21 @@ public class LinkedListDemo {
 		ll.get(0);
 		for (String s : ll) {
 			System.out.println(s);
-		}
-
+		}*/
+		
+		public static void main(String[] args) {
+			MyLinkedList<String> ll = new MyLinkedList<>();
+			ll.add("Hallo");
+			ll.add("Welt");
+			ll.add("Java");
+			ll.add(1, "neues Element");
+			System.out.println("Size: " + ll.size());
+			System.out.println("Element at index 2: " + ll.get(2));
+			ll.remove(0);
+			ll.set(1, "geändert");
+			for (String s : ll) {
+				System.out.println(s);
+			}
 	}
 
 }
