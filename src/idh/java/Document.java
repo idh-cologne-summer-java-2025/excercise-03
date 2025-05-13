578ddc0d24f742a233c93af136602ab55a50339d
karim <karim@air-von-karim>
2025-05-13 22:46:21 +0200

    von tennisdennis5

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -4,27 +4,25 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class Document{
+public class Document implements Iterable<String> {
 	String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
 		FileReader fileReader = new FileReader(f);
 		int ch;
 		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
+		while ((ch = fileReader.read()) != -1) {
 			b.append((char) ch);
 		}
 		fileReader.close();
 		Document doc = new Document();
 		doc.documentText = b.toString();
-		
+
 		return doc;
 	}
-	
+
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -32,12 +30,29 @@ public class Document{
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
-	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		
+
+	@Override
+	public Iterator<String> iterator() {
+		return new Iterator<String>() {
+			StringTokenizer tokenizer = new StringTokenizer(documentText);
+
+			@Override
+			public boolean hasNext() {
+				return tokenizer.hasMoreTokens();
+			}
+
+			@Override
+			public String next() {
+				return tokenizer.nextToken();
+			}
+		};
 	}
 
-	
-	
+	public static final void main(String[] args) throws IOException {
+		Document d = Document.readFromFile(new File("data/dracula.txt"));
+
+		for (String token : d) {
+			System.out.println(token);
+		}
+	}
 }
