2117a49ce07a9188547143f80560646d2df7bb77
mariasarkisian <mariasarkisian@localhost>
2025-05-13 23:40:54 +0200

    exercise-03 gelöst

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,12 +3,14 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Document{
+public class Document implements Iterable <String>{
+	
 	String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
@@ -35,7 +37,21 @@ public class Document{
 	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		
+		Iterator<String> iterator = d.iterator(); // das d definiert was der iterator definiert
+		while(iterator.hasNext())
+		//ALLTERNATIVE: for (String word: d) {
+	    // System.out.println(iterator.next()); }
+		{
+			System.out.println(iterator.next());
+		}
+	    }
+
+	public Iterator<String> iterator() {
+		StringTokenizer str = new StringTokenizer(documentText);
+		List<String> list= new ArrayList<String>();
+		while (str.hasMoreTokens());
+		list.add(str.nextToken());
+		return null;
 	}
 
 	
