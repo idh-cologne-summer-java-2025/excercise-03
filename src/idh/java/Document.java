27ff9b91c9a9f260abb48d35911fdbea778ca9ab
faran230 <130466482+faran230@users.noreply.github.com>
2025-05-13 23:09:39 +0200

    Aufgabe 1

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -4,40 +4,55 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class Document{
-	String documentText;
+public class Document implements Iterable<String> {
+    String documentText;
 
-	public static Document readFromFile(File f) throws IOException {
-		FileReader fileReader = new FileReader(f);
-		int ch;
-		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
-			b.append((char) ch);
-		}
-		fileReader.close();
-		Document doc = new Document();
-		doc.documentText = b.toString();
-		
-		return doc;
-	}
-	
-	public String getDocumentText() {
-		return documentText;
-	}
+    public static Document readFromFile(File f) throws IOException {
+        FileReader fileReader = new FileReader(f);
+        int ch;
+        StringBuilder b = new StringBuilder();
+        while( (ch = fileReader.read()) != -1 ) {
+            b.append((char) ch);
+        }
+        fileReader.close();
+        Document doc = new Document();
+        doc.documentText = b.toString();
 
-	public void setDocumentText(String documentText) {
-		this.documentText = documentText;
-	}
-	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		
-	}
+        return doc;
+    }
 
-	
-	
+    public String getDocumentText() {
+        return documentText;
+    }
+
+    public void setDocumentText(String documentText) {
+        this.documentText = documentText;
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return new Iterator<String>() {
+            private StringTokenizer tokenizer = new StringTokenizer(documentText);
+
+            @Override
+            public boolean hasNext() {
+                return tokenizer.hasMoreTokens();
+            }
+
+            @Override
+            public String next() {
+                return tokenizer.nextToken();
+            }
+        };
+    }
+
+    public static final void main(String[] args) throws IOException {
+        Document d = Document.readFromFile(new File("data/dracula.txt"));
+
+        for (String token : d) {
+            System.out.println(token);
+        }
+    }
 }
