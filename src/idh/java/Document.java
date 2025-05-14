fc35441065ecdc5ab9599328d94e136b6f10950d
chihayabu <chihayabu@proton.me>
2025-05-14 04:11:43 +0200

    Update Document.java

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Document{
+public class Document implements Iterable<String>{
 	String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
